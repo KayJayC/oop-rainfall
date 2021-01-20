b96980832a855e461d749e77b42e7275f5229358
Kayla <67656136+KayJayC@users.noreply.github.com>
2021-01-20 13:58:22 -0500

    reverted code back to before changes were made

--- OOPS21-Rainfall-master/rainfall.cpp
+++ OOPS21-Rainfall-master/rainfall.cpp
@@ -1,42 +1,29 @@
-/*
-    rainfall.cpp
-
-    Rainfall report. Individual rain amounts per hour are input. Output is the average
-    and heaviest hourly rainfall.
-*/
-
 #include <iostream>
 #include <iomanip>
 #include <vector>
 
-int main() {
+using namespace std;
 
-    // input hourly rainfall data
-    std::vector<double> rainfall;
-    double n;
-    while (std::cin >> n) {
+int main() {
+    vector<float> rainfall;
+    float t;
+    float m;
+    float n;
+
+    while (cin >>n){
         rainfall.push_back(n);
     }
-    if (!rainfall.size())
-        return 1;
-
-    // calculate average rainfall
-    auto total = rainfall[0];
-    for (int i = 1; i < rainfall.size(); ++i) {
-        total += rainfall[i];
-    }
-    auto average = total / rainfall.size();
-
-    // calculate heaviest rainfall
-    auto max = rainfall[0];
-    for (int i = 1; i < rainfall.size(); ++i) {
-        if (rainfall[i] > max)
-            max = rainfall[i];
+    t= rainfall[0];
+    m= rainfall[0];
+    for (int i=1; i < rainfall.size(); ++i){
+        t+= rainfall [i];
+        if (rainfall[i] > m)
+            m= rainfall [i];
     }
 
     // output the rainfall report
-    std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << average << " hundreds of inches" << '\n';
-    std::cout << "Heaviest Hourly Rainfall: " << max << " hundreds of inches" << '\n';
+    std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << (t/rainfall.size()) << " hundreds of inches" << '\n';
+    std::cout << "Heaviest Hourly Rainfall: " << m << " hundreds of inches" << '\n' ;
 
     return 0;
 }
