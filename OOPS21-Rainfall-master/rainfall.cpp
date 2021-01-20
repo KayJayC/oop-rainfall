feca8a3f6fd83c125a911b2632c192896f394981
Kayla <67656136+KayJayC@users.noreply.github.com>
2021-01-20 15:22:31 -0500

    Replace loop with std:: accumulate()

--- OOPS21-Rainfall-master/rainfall.cpp
+++ OOPS21-Rainfall-master/rainfall.cpp
@@ -8,26 +8,16 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <numeric>
 
 //average of rainfall data
 double average(const std::vector<double>& data){
-    auto total= data[0];
-    for (int i = 1; i < data.size(); ++i){
-        total += data [i];
-    }
-    return total /data.size();
+    return std::accumulate (data.begin(), data.end(), 0.0)/data.size();
 }
 
 //heaviest Rainfall
 double heaviest(const std::vector<double>& data){
     return *std::max_element (data.begin(), data.end());
-
-    auto max= data[0];
-    for (int i=1; i < data.size(); ++i){
-        if (data[i] > max)
-            max= data[i];
-    }
-    return max;
 }
 
 
