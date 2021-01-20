043d9cc4eb5dd478c1d0c26f9cf3e3afa555fe72
Kayla <67656136+KayJayC@users.noreply.github.com>
2021-01-20 15:18:00 -0500

    replaced loop with std::max_element()

--- OOPS21-Rainfall-master/rainfall.cpp
+++ OOPS21-Rainfall-master/rainfall.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <vector>
 #include <iterator>
+#include <algorithm>
 
 //average of rainfall data
 double average(const std::vector<double>& data){
@@ -19,6 +20,8 @@ double average(const std::vector<double>& data){
 
 //heaviest Rainfall
 double heaviest(const std::vector<double>& data){
+    return *std::max_element (data.begin(), data.end());
+
     auto max= data[0];
     for (int i=1; i < data.size(); ++i){
         if (data[i] > max)
@@ -35,15 +38,6 @@ int main() {
                                   std::istream_iterator<double>() };
     if (!rainfall.size())
         return 1;
-
-    /*calculate heaviest rainfall
-    auto max= rainfall[0];
-    for (int i=1; i < rainfall.size(); ++i){
-        if (rainfall[i] > max)
-            max= rainfall [i];
-    }
-    max = heaviest (rainfall);
-    */
     // output the rainfall report
     std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << average(rainfall) << " hundreds of inches" << '\n';
     std::cout << "Heaviest Hourly Rainfall: " << heaviest(rainfall) << " hundreds of inches" << '\n' ;
